b02fea6fbf15b2ad61aa9cff2e0ff8d60a89c02e
Matt Gordon <bigmattsize21@gmail.com>
2020-11-12 00:42:31 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: ->(user_data){ user_data[:username].blank?}
+
+  def username
+    self.user.username if not self.user.nil?
+  end
 
 end
